654e8edc94f3753b65acc7fabf80530b13e31784
Adler Sebastian <adlersebastianj@gmail.com>
2025-06-16 11:33:18 -0300

    consertando o next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  experimental: {},
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NEXT_PUBLIC_API_URL,
+      },
+    ];
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
 
 export default nextConfig;
